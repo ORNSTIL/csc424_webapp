14eec59e1c19c46ca22251ce90096b543eea7283
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-01-17 15:27:41 -0800

    Update user.js

--- csc424-react-app/expressjs-backend/models/user.js
+++ csc424-react-app/expressjs-backend/models/user.js
@@ -3,19 +3,10 @@ const { mongoose } = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
   {
-    name: {
+    username: {
       type: String,
       required: true,
       trim: true,
-    },
-    job: {
-      type: String,
-      required: true,
-      trim: true,
-      validate(value) {
-        if (value.length < 2)
-          throw new Error("Invalid job, must be at least 2 characters.");
-      },
     },
 	password: {
       type: String,
