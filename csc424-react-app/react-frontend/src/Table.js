bfb9299978665022e05f52e7ed11da9d26325bee
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-01-29 12:57:12 -0800

    Create Table.js

---
+++ csc424-react-app/react-frontend/src/Table.js
@@ -0,0 +1,30 @@
+import { useAuth } from "./context/AuthProvider.js";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import Table from './Table.js';
+
+export const Landing = () => {
+  const { value } = useAuth();
+  const [tableUsers, setTableUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:8000/account/users'
+    ).then( response => {
+      if(response){
+        setTableUsers(response.data);
+      }
+    });
+  },[setTableUsers]);
+
+  return (
+    <>
+      <h2>Landing (Protected)</h2>
+      <div> Authenticated as {value.token}</div>
+      <Table contacts={tableUsers} />
+    </>
+  );
+};
+
+export default Landing;
