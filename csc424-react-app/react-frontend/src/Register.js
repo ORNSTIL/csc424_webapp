7aded6124dd9f67ddd6650061f7f6a0e254d4a22
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-01-29 12:55:33 -0800

    Update Register.js

--- csc424-react-app/react-frontend/src/Register.js
+++ csc424-react-app/react-frontend/src/Register.js
@@ -1,63 +1,79 @@
-
-
-import { useAuth } from "./context/AuthProvider";
-import React, { useState } from "react";
-
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useAuth } from "./context/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
 
 export const Register = () => {
-    const { value } = useAuth();
-    const navigate = useNavigate();
+    const { value } = useAuth(); 
+    const [username, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+	const navigate = useNavigate();
 
-	const [registerUser, setRegisterUser] = useState("");
-	const [registerPass, setRegisterPass] = useState("");
-	const [registerValidatedPass, setRegisterValidatedPass] = useState("");
-
-
-const handleRegister = async () => {
-    try {
-        const response = await axios.post('http://localhost:8000/account/register', {
-            username: registerUser,
-            password: registerPass,
-            validatePassword: registerValidatedPass
-        });
-        navigate("/landing");
-    } catch (error) {
-        alert(error.response.data);
-        console.log(error);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+		try {
+            if(password === confirmPassword){
+            
+            let user = {
+                username: username,
+                phoneNumber: phoneNumber,
+                password: password,
+                //confirmPassword: confirmPassword
+            } 
+            const response = await axios.post('https://localhost:8000/account/register', {user});
+            console.log(response);
+            if(response.status === 201){
+				console.log("yo");
+                value.onRegister(response.data);
+				//navigate("/landing");
+            }
+            else{
+                alert("Register failed.");
+            }
+        }
+        else{
+            alert("Passwords do not match.");
+        }
+        } catch (error) {
+            alert(JSON.stringify(error.response.data.message));
+        }
     }
-};
-
-
-
 
     return (
-    <>	
-	  <div>
-    <h2>Register (Public)</h2>
-
-    <div>
-      <label htmlFor="Username">Username:</label>
-      <input type="text" id="registerUser" />
-    </div>
-
-    <div>
-      <label htmlFor="Password1">Password:</label>
-      <input type="password" id="registerPass" />
-    </div>
-
-	 <div>
-      <label htmlFor="Password2">Confirm Password:</label>
-      <input type="password" id="registerValidatedPass" />
-    </div>
-	
-    <div>
-      <button type="button" onClick={handleRegister}>
-        Register
-      </button>
-    </div>
-  </div>
-    </>
-);
+        <>
+        <div>
+            <h2>Sign Up</h2>
+            <form>
+                <p>Username</p>
+                <input 
+                    type="text" 
+                    name="Username" 
+                    value={username} onChange={e => setName(e.target.value)}/>
+                <p>Phone Number</p>
+                <input 
+                    type="text" 
+                    name="Phone Number" 
+                    value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
+                <p>Password</p>
+                <input 
+                    type="password" 
+                    name="Password" 
+                    value={password} onChange={e => setPassword(e.target.value)}/>
+                <p>Confirm Password</p>
+                <input 
+                    type="password" 
+                    name="Confirm Password" 
+                    value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
+            </form>
+            <button type="submit" onClick={handleSubmit}>
+                Sign Up
+            </button>
+        </div>
+        </>
+    );
 };
+
+export default Register;
