b03ff566989bcf2016677147ba26b43728a95320
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-01-30 21:40:48 -0800

    Update Landing.js

--- csc424-react-app/react-frontend/src/Landing.js
+++ csc424-react-app/react-frontend/src/Landing.js
@@ -6,16 +6,25 @@ import Table from './Table';
 export const Landing = () => {
   const { value } = useAuth();
   const [tableUsers, setTableUsers] = useState([]);
-  
- /* async function fetchAll() {
+
+  /*useEffect(() => {
+    axios.get('https://localhost:8000/account/users'
+    ).then( response => {
+      if(response){
+        setTableUsers(response.data);
+      }
+    });
+  },[setTableUsers]);
+  */
+  async function fetchAll() {
    /* if (token) console.log("fetchall Authorization: Bearer " + token);
     else {
       token = value.token;
       console.log("fetchall Authorization: Bearer " + value.token);
-    }
-	console.log("in fetch all");
+    }*/
+	//console.log("in fetch all");
     try {
-		console.log(value.token);
+	//	console.log(value.token);
       const response = await axios.get(
         'https://localhost:8000/account/users',
         {
@@ -23,30 +32,29 @@ export const Landing = () => {
         }
       );
 	  console.log(response.data);
-      return response.data;
+      return response;
     } catch (error) {
       //We're not handling errors. Just logging into the console.
       console.log(error);
       return false;
     }
   }
-*/
+
   useEffect(() => {
-	  console.log("in use effect");
-    /*fetchAll().then( response => {
-		console.log("ayo");
+	//  console.log("in use effect");
+    fetchAll().then( response => {
+		//console.log("ayo");
       if(response){
-		  console.log(response.data);
+		 console.log(response.data);
+		 console.log("space");
         setTableUsers(response.data);
 		console.log(tableUsers);
       }
-	  else {
-		  console.log("no");
-	  }
-    });*/
-  },[]);
-console.log("in landing");
-console.log(value.token);
+
+    });
+  },[tableUsers]);
+//console.log("in landing");
+//console.log(value.token);
   return (
     <>
       <h2>Landing (Protected)</h2>
