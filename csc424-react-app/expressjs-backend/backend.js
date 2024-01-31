6fbabc8782870c7f43956cceb187c49b3b47de32
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-01-30 21:29:29 -0800

    Update backend.js

--- csc424-react-app/expressjs-backend/backend.js
+++ csc424-react-app/expressjs-backend/backend.js
@@ -3,7 +3,6 @@ const app = express();
 const port = 8000;
 const cors = require('cors');
 
-
 const userServices = require('./models/user-services');
 const jwtToken = require('./models/jwt-token');
 
@@ -15,10 +14,44 @@ const { body, validationResult } = require('express-validator');
 app.use(cors());
 app.use(express.json());
 
+app.get('/account/users', async (req, res) => {
 
+	console.log("made it to the get in backend");
+    var allInfo = await userServices.getAllUsers();
+    var result = allInfo.map(user => {
+        return {name: user.username, phoneNumber: user.phoneNumber}
+    })
+	console.log("sending users");
+    res.status(201).send(result);
+});
+
+app.get('account/users/:username', [
+    body('username').isAlphanumeric().isLength({ min: 1, max: 20 })
+], async (req, res) => {
+
+    const errors= validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(405).json({ errors: errors.array() });
+    }
+
+    let Username = req.params['username'];
+
+    const result = await userServices.findUserByUsername(Username);
+    
+    if(result === undefined || result.length == 0){
+        res.status(404).send('Resource not found.');
+    }
+    else{
+
+        let encodedResult = encodeURI(result);
+        res.status(201).send(encodedResult);
+
+
+    }
+});
 
 app.post('/account/login', async (req, res) => {
-// app.post('/account/login', jwtToken.authenticateToken, async (req, res) => {
+
 
     try{
         const username = req.body.value.username;
@@ -28,7 +61,7 @@ app.post('/account/login', async (req, res) => {
         if(user){
             const token = jwtToken.generateAccessToken({ username: username });
             res.status(201).send({username, password, token});
-            // res.status(201).send(token);
+
 
         }
         else{
@@ -44,14 +77,9 @@ app.post('/account/register', async (req, res) => {
     const User = req.body.user;
 
     const Username = req.body.user.username;
-    //const PhoneNumber = req.body.user.phoneNumber
-    const Password = req.body.user.password;
-    //const confirmPassword = req.body.user.confirmPassword;
 
-    // if(Password != confirmPassword){
-    //     res.status(400).json({message: 'Passwords do not match'});
-    // }
-    //else if(!containsUppercase(Password)){
+    const Password = req.body.user.password;
+
     if(!containsUppercase(Password)){
         res.status(400).json({message: 'Passwords require an uppercase letter'});
     }
@@ -74,47 +102,7 @@ app.post('/account/register', async (req, res) => {
     }
 });
 
-app.get('/account/users', async (req, res) => {
-    ////res.send(account);
-    // const result = await userServices.getAllUsers();
-	
-	// console.log('Received GET request for users');
-   // res.status(200).send('Hello from the backend!');
-	
-	console.log("made it to the get in backend");
-    var allInfo = await userServices.getAllUsers();
-    var result = allInfo.map(user => {
-        return {name: user.username, phoneNumber: user.phoneNumber}
-    })
-	console.log("sending users");
-    res.status(201).send(result);
-});
 
-app.get('account/users/:username', [
-    body('username').isAlphanumeric().isLength({ min: 1, max: 20 })
-], async (req, res) => {
-
-    const errors= validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(405).json({ errors: errors.array() });
-    }
-
-    let Username = req.params['username'];
-    ////let result = findUserByUsername(username);
-    const result = await userServices.findUserByUsername(Username);
-    
-    if(result === undefined || result.length == 0){
-        res.status(404).send('Resource not found.');
-    }
-    else{
-        ////result = {account_list: result};
-        ////res.send(result);
-        let encodedResult = encodeURI(result);
-        res.status(201).send(encodedResult);
-
-        // res.status(201).send(result);
-    }
-});
 
 
 function containsUppercase(str) {
@@ -128,9 +116,7 @@ function containsSpecialChar(str) {
     return format.test(str);
 }
 
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`);
-// });      
+    
 
 https
   .createServer(
