34d592f550cd096442e017b2a5b6e1fe864f8c34
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-02-11 18:36:50 -0800

    Update user.js

--- csc424-react-app/expressjs-backend/models/user.js
+++ csc424-react-app/expressjs-backend/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
   {
@@ -7,23 +7,28 @@ const UserSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
-
     password: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (value.length < 2)
+          throw new Error("Invalid password, must be at least 2 characters.");
+      },
     },
-    
-    phoneNumber: {
+    phone: {
       type: String,
       required: true,
       trim: true,
     },
-
+    token: {
+      type: String,
+      required: true,
+      trim: true,
+    }
   },
-  { collection: "users_list" }
+  { collection: "users_list" },
 );
 
 const User = mongoose.model("User", UserSchema);
-
 module.exports = User;
