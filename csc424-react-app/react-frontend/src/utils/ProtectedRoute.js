201a23e430f9ed16a2a9122c92ade34a0cc35ec0
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-02-11 18:28:31 -0800

    Update ProtectedRoute.js

--- csc424-react-app/react-frontend/src/utils/ProtectedRoute.js
+++ csc424-react-app/react-frontend/src/utils/ProtectedRoute.js
@@ -1,13 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
-import React from "react";
 
 export const ProtectedRoute = ({ children }) => {
   const { value } = useAuth();
-  if (!value.token) {
+  const token = document.cookie && document.cookie.split("=")[1];
+  if (token === "null") {
     return <Navigate to="/home" replace />;
   }
   return children;
 };
-
-export default ProtectedRoute;
