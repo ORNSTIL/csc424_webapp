22cb285ea5b497b5f30fb1e112deb6ba50cbc363
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-02-11 18:31:28 -0800

    Update Register.js

--- csc424-react-app/react-frontend/src/Register.js
+++ csc424-react-app/react-frontend/src/Register.js
@@ -1,80 +1,51 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
 import { useAuth } from "./context/AuthProvider";
+import { HandleRegistrationAttempt } from "./apihelper";
 import { useNavigate } from "react-router-dom";
 
+const Register = () => {
+  const { value } = useAuth();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [phone, setPhone] = useState("");
 
-export const Register = () => {
-    const { value } = useAuth(); 
-    const [username, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-	const navigate = useNavigate();
+  const handleSubmit = () => {
+    value.username = username;
+    value.password = password;
+    value.phone = phone;
+    HandleRegistrationAttempt({ username: value.username, password: value.password, password2: password2, phone: value.phone })
+      .then((res) => res.json())
+      .then((res) => {
+        value.onLogin(res.token);
+        navigate("/landing");
+      })
+      .catch((exception) => console.log(exception));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-		try {
-            if(password === confirmPassword){
-            
-            let user = {
-                username: username,
-                phoneNumber: phoneNumber,
-                password: password,
-                //confirmPassword: confirmPassword
-            } 
-            const response = await axios.post('https://localhost:8000/account/register', {user});
-            console.log(response);
-            if(response.status === 201){
-				console.log("yo");
-                //value.onRegister(response.data);
-				value.onRegister(response.data);
-				//navigate("/landing");
-            }
-            else{
-                alert("Register failed.");
-            }
-        }
-        else{
-            alert("Passwords do not match.");
-        }
-        } catch (error) {
-            alert(JSON.stringify(error.response.data.message));
-        }
-    }
-
-    return (
-        <>
-        <div>
-            <h2>Sign Up</h2>
-            <form>
-                <p>Username</p>
-                <input 
-                    type="text" 
-                    name="Username" 
-                    value={username} onChange={e => setName(e.target.value)}/>
-                <p>Phone Number</p>
-                <input 
-                    type="text" 
-                    name="Phone Number" 
-                    value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
-                <p>Password</p>
-                <input 
-                    type="password" 
-                    name="Password" 
-                    value={password} onChange={e => setPassword(e.target.value)}/>
-                <p>Confirm Password</p>
-                <input 
-                    type="password" 
-                    name="Confirm Password" 
-                    value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)}/>
-            </form>
-            <button type="submit" onClick={handleSubmit}>
-                Sign Up
-            </button>
-        </div>
-        </>
-    );
+  return (
+    <>
+      <h2>Register (Public)</h2>
+      <form>
+        <label>
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          <input type="text" placeholder="Phone #" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </label>
+        <label>
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <label>
+          <input type="password" placeholder="Confirm Password" value={password2} onChange={(e) => setPassword2(e.target.value)} />
+        </label>
+        <button type="button" onClick={handleSubmit}>
+          Sign Up
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Register;
