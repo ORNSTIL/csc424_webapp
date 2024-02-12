6b15cad07158c6d5d2956559f39674aef1b1173b
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-02-11 18:31:03 -0800

    Update Landing.js

--- csc424-react-app/react-frontend/src/Landing.js
+++ csc424-react-app/react-frontend/src/Landing.js
@@ -1,67 +1,28 @@
+import React, { useEffect, useState } from "react";
 import { useAuth } from "./context/AuthProvider";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Table from './Table';
+import { getUsers } from "./apihelper";
+import Table from "./Table";
 
-export const Landing = () => {
+const Landing = () => {
   const { value } = useAuth();
-  const [tableUsers, setTableUsers] = useState([]);
-
-  /*useEffect(() => {
-    axios.get('https://localhost:8000/account/users'
-    ).then( response => {
-      if(response){
-        setTableUsers(response.data);
-      }
-    });
-  },[setTableUsers]);
-  */
-  async function fetchAll() {
-   /* if (token) console.log("fetchall Authorization: Bearer " + token);
-    else {
-      token = value.token;
-      console.log("fetchall Authorization: Bearer " + value.token);
-    }*/
-	//console.log("in fetch all");
-    try {
-	//	console.log(value.token);
-      const response = await axios.get(
-        'https://localhost:8000/account/users',
-        {
-          headers: { Authorization: `bearer ${value.token}` },
-        }
-      );
-	  console.log(response.data);
-      return response;
-    } catch (error) {
-      //We're not handling errors. Just logging into the console.
-      console.log(error);
-      return false;
-    }
-  }
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-	//  console.log("in use effect");
-    fetchAll().then( response => {
-		//console.log("ayo");
-      if(response){
-		 console.log(response.data);
-		 console.log("space");
-        setTableUsers(response.data);
-		console.log(tableUsers);
-      }
+    const token = document.cookie.split("=")[1];
+    getUsers(token)
+      .then((res) => res.json())
+      .then((json) => setCharacters(json))
+      .catch((error) => console.log(error));
+  }, []);
 
-    });
-  },[tableUsers]);
-//console.log("in landing");
-//console.log(value.token);
   return (
     <>
       <h2>Landing (Protected)</h2>
-      <div>Authenticated as {value.token}</div>
-      <Table contacts={tableUsers} />
+      <Table characterData={characters} />
+      <div> Authenticated as {document.cookie.split("=")[1]}</div>
     </>
   );
 };
 
 export default Landing;
+
